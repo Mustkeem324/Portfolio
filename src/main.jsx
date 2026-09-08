47390b8b280a55670065015dbeba3f86ce0c925a
MUSTKEEM AHMAD <46074466+Mustkeem324@users.noreply.github.com>
2026-09-08 12:14:17 +0530

    Mount work opportunity popup globally

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,12 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import App from './App.jsx'
+import WorkOpportunityPopup from './components/WorkOpportunityPopup.jsx'
 import './index.css'
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
     <App />
+    <WorkOpportunityPopup />
   </StrictMode>,
 )
